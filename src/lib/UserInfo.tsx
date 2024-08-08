a41f935103dc4f2036fd34232264c301873934cd
ismailemayej <salikbd.com@gmai.com>
2024-08-08 20:02:55 +0600

    login update

--- src/lib/UserInfo.tsx
+++ src/lib/UserInfo.tsx
@@ -1,4 +1,4 @@
-import { userInformation } from "@/utils/LocalStore";
+import { userInformation } from "@/components/DataAction/DataHandle";
 
 export const UserInfo = async () => {
   const user = await userInformation();
