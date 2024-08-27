c204eed62a5048a7c8a11ab4f4c7bffe9a4c8ce6
ismailemayej <salikbd.com@gmai.com>
2024-08-28 05:01:06 +0600

    update

--- src/app/(protectedPage)/login/LoginUser.tsx
+++ src/app/(protectedPage)/login/LoginUser.tsx
@@ -29,7 +29,7 @@ const SignIn = () => {
   }, [state, ref, router]);
   return (
     <div className=" lg:m-2 p-2 lg:px-4 rounded-xl">
-      <div className="grid lg:grid-cols-2 gap-10">
+      <div className="grid lg:grid-cols-2 lg:gap-10">
         <div className="lg:block hidden">
           <Image
             className=""
